86dd997f97dd16ecc7b3b506e03fa683dac3708e
Tom Chen <tom@itri.org.tw>
2019-10-22 22:42:47 +0800

    Add apollo client call graphql query API

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// 解析graphQL 查詢文檔
+import gql from 'graphql-tag';
+// 發起graphQL 查詢請求
+import { useQuery } from 'react-apollo-hooks';
+
+const queryAllUsers = gql`
+  query($first:Int) {
+    users(first: $first) {
+      id
+      name
+      email
+      password
+    }
+  }
+`
+
 function App() {
+
+  const { data, loading, error } = useQuery(queryAllUsers);
+  
+  if (loading) {
+    return <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          loading!!! 
+        </p>
+      </header>
+    </div>
+  }
+  if (error) {
+    return <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          錯誤發生了!!
+      </header>
+    </div>
+  }
+  
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +50,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          我只是要測試前端呼叫後端 graphQL 的使用方式而已。
+          前端測試呼叫後端 graphQL API
         </p>
         <a
           className="App-link"
